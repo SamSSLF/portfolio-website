e820c800045ef2578bfdfaebc0788ff8a618f57d
Samantha Foong <shenli574@gmail.com>
2023-02-19 21:49:07 +0000

    project gallery centered titles

--- src/components/ProjectCard.vue
+++ src/components/ProjectCard.vue
@@ -8,15 +8,18 @@ const props = defineProps({
 <template>
   <RouterLink
     :to="'/projects/' + project.shortTitle"
-    class="group block aspect-w-16 aspect-h-9 col-span-1 lg:col-span-2 bg-white shadow rounded-lg overflow-hidden hover:cursor-pointer hover:scale-105 transition"
+    class="group block aspect-w-16 aspect-h-9 col-span-1 lg:col-span-2 bg-white shadow overflow-hidden hover:cursor-pointer transition"
   >
-    <p
-      class="text-center top-24 text-xl z-50 hidden group-hover:block font-raleway font-bold transition"
-    >
-      {{ project.title }}
-    </p>
+    <div class="flex flex-col items-center justify-center">
+      <p
+        class="text-center text-gray-900 text-md lg:text-lg z-50 hidden group-hover:block font-raleway font-semibold transition"
+      >
+        {{ project.title }}
+      </p>
+    </div>
+
     <img
-      class="object-cover group-hover:opacity-30 transition"
+      class="object-cover group-hover:opacity-10 transition"
       :src="project.imageSrc"
       :alt="project.imageAlt"
     />
