a1bbe70bf29a0c1f294e23a1b674398e968c1761
Samantha Foong <shenli574@gmail.com>
2023-02-20 00:08:20 +0000

    adding dist subtree

---
+++ dist/assets/Contact.56eb61e7.js
@@ -0,0 +1,1 @@
+import{_ as e}from"./_plugin-vue_export-helper.cdc0426e.js";import{v as t,x as o,y as c}from"./index.b2227315.js";const s={},n=c("h1",null,"This is a contact page",-1),a=[n];function r(_,i){return t(),o("div",null,a)}const f=e(s,[["render",r]]);export{f as default};
