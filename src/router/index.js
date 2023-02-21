590d50e50cac3fb3b06ec6cfdc71773e18d3dd49
Samantha Foong <shenli574@gmail.com>
2023-02-21 22:47:33 +0000

    scroll to top and saved position

--- src/router/index.js
+++ src/router/index.js
@@ -3,6 +3,13 @@ import HomeView from "../views/Home.vue";
 import ProjectGalleryView from "../views/ProjectGallery.vue";
 
 const router = createRouter({
+  scrollBehavior(to, from, savedPosition) {
+    if (savedPosition) {
+      return savedPosition;
+    } else {
+      return { top: 0 };
+    }
+  },
   history: createWebHistory(import.meta.env.BASE_URL),
   linkActiveClass: "text-black ",
   routes: [
