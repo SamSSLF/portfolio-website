f044c798ce66453e870cdaf855b1068e8d4ee343
SamSSLF <56586268+SamSSLF@users.noreply.github.com>
2023-09-12 00:52:50 +0100

    updated CV

--- src/views/CV.vue
+++ src/views/CV.vue
@@ -1,9 +1,11 @@
 <template>
   <div class="col-start-2">
     <div class="text-indigo-700 font-medium tracking-widest text-sm hover:underline hover:underline-offset-4">
-      <a href="https://drive.google.com/u/0/uc?id=1Yqgv9dKw47Py8PwjpW3WiWmonC54nBI2&export=download" download>DOWNLOAD MY CV</a>
+      <a href="https://drive.google.com/u/0/uc?id=1Fd1Dkz0jFQLsk4Vb0zWqphdxHVWRPBQK&export=download" download>DOWNLOAD MY CV</a>
     </div>
 
-    <iframe src="https://drive.google.com/file/d/1Yqgv9dKw47Py8PwjpW3WiWmonC54nBI2/preview" height="680" allow="autoplay" class="mt-4 w-full"></iframe>
+    <iframe src="https://drive.google.com/file/d/1Fd1Dkz0jFQLsk4Vb0zWqphdxHVWRPBQK/preview" height="680" allow="autoplay" class="mt-4 w-full"></iframe>
   </div>
 </template>
+
+
