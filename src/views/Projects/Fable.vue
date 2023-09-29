8c58510b5dd329dbd01178e81d1164cd2f619228
Samantha Foong <shenli574@gmail.com>
2023-09-29 15:22:52 +0100

    Some Fable

--- src/views/Projects/Fable.vue
+++ src/views/Projects/Fable.vue
@@ -1,13 +1,110 @@
 <template>
-  <div class="col-start-2 col-span-1">
-    <div>
-      <div class="flex flex-col items-center justify-center">
-        <img
-          src="https://media.tenor.com/pPoUmi0Z1fUAAAAi/cat-pet.gif"
-          class="h-full"
-        />
-        <p class="text-2xl font-bold text-center">Content Under Construction</p>
-      </div>
-    </div>
-  </div>
+  <article
+    class="col-start-2 col-span-1 prose prose-h1:text-4xl prose-h1:font-bold prose-h1:mb-2 prose-p:prose-md prose-h3:font-normal"
+  >
+    <h1>
+      Fable - A language-learning toy that supports the development of
+      self-efficacy in children aged 3-7 years.
+    </h1>
+    <h3 class="mb-4">
+      Fable is a digital-physical toy which aims to improve second language
+      proficiency by leveraging self-efficacy theory (SET) as a mechanism that
+      could help explain why some children thrive learning a second language
+      whereas others struggle.
+    </h3>
+    <h3>
+      The toy is underpinned by Bandura’s theory of self-efficacy, and current
+      theories on intellectual development across the lifespan. An initial
+      prototype was developed and tested with potential users and areas of
+      improvement were noted for future iterations. As part of my Design
+      Psychology module, an experimental design to test the toy’s claim that it
+      can improve second language proficiency is outlined in a report that can
+      be read here.
+    </h3>
+    <p class="mb-8">
+      <span class="font-bold">Time Frame: </span>5 weeks<br />
+      <span class="font-bold"> Team: </span>
+      Paula Castillero, Kyle Li, Irina Barzykina, Anna Soligo and myself.<br />
+      <span class="font-bold"> Role: </span>I was responsible for the initial
+      concept development, user research, user testing, user interface design,
+      and design psychology research for development. I am the sole author of
+      the proposed research study to test the toy's efficacy empirically.
+    </p>
+  </article>
+  <img src="../../assets/fable-hero-4.png" alt="" class="col-span-full" />
+  <p
+    class="col-start-2 text-sm mt-3 tracking-wide font-semibold text-slate-500"
+  >
+    Mock-up of the final toy design. Fable consists of a set of square, magnetic
+    tiles, and a companion app which connects to the tiles over bluetooth.
+  </p>
+  <article
+    class="mb-2 col-start-2 col-span-1 prose prose-h1:text-4xl prose-h1:font-bold prose-h1:mb-2 prose-h1:mt-10 prose-p:prose-md prose-h3:font-normal prose-h4:mb-3"
+  >
+    <h1>Background and Context</h1>
+    <p>
+      The development of Fable stemmed from the observation that low proficiency
+      in one’s heritage language prevents one from engaging meaningfully with
+      your ethnic background, culture, and extended family who can only speak
+      the heritage language.
+    </p>
+    <p>
+      Language proficiency is closely correlated to an individual’s self-esteem
+      and sense of identity, and bilingual children have also shown to have
+      superior executive functioning skills compared to monolinguals and their
+      brains are more resilient against age-related decline. Thus, second
+      language proficiency can improve mental health of immigrant children and
+      generally comes with many cognitive benefits that persist into adulthood.
+    </p>
+    <p>
+      During the pre-operational stage of development, ages 3-7 years children
+      begin to form complex sentences and maintain complex interactions - beyond
+      the age of 7, children tend to focus more on reading and writing in
+      schools. Our toy intends to target children at this key stage of their
+      development.
+    </p>
+    <p>This project can be summarised as follows:</p>
+
+    <!-- Quotation Start-->
+    <p class="text-gray-600 text-3xl mb-4">
+      <font-awesome-icon
+        :icon="['fas', 'quote-left']"
+        size="2xl"
+        class="text-gray-100 absolute z-0"
+      />
+      <i class="relative left-4 top-4 z-10"
+        >How might we apply psychological principles to create an engaging
+        language-learning toy for children aged 3-7 years?</i
+      >
+      <font-awesome-icon
+        :icon="['fas', 'quote-right']"
+        size="2xl"
+        class="text-gray-100 absolute z-0"
+      />
+    </p>
+    <!--Quotation End -->
+  </article>
+  <article
+    class="mb-2 col-start-2 col-span-1 prose prose-h1:text-4xl prose-h1:font-bold prose-h1:mb-2 prose-h1:mt-10 prose-p:prose-md prose-h3:font-normal prose-h4:mb-3"
+  >
+    <h1>Insights from Psychology</h1>
+    <ul>
+      <li>
+        <span class="font-bold">Self-Efficacy:</span> It's the belief in our
+        ability to succeed in specific situations. It makes learners more
+        persistent, especially in challenging tasks like language learning.
+      </li>
+      <li>
+        <span class="font-bold">Parental Value:</span> If parents don’t see the value in learning a new
+        language, a child is less likely to put in the effort to learn it,
+        regardless of their self-efficacy.
+      </li>
+      <li>
+        <span class="font-bold">Parental Input:</span> The quality and diversity of language that parents use
+        with their children can impact the child’s language development.
+        Activities like reading together can especially help in improving
+        language skills in children.
+      </li>
+    </ul>
+  </article>
 </template>
