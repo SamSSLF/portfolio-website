39f0ad531d137d1204c4f6b62ab1a7139b8d6827
Samantha Foong <shenli574@gmail.com>
2023-10-08 13:44:32 +0100

    Updated Fable

--- src/views/Projects/Fable.vue
+++ src/views/Projects/Fable.vue
@@ -1,4 +1,11 @@
 <template>
+  <img src="../../assets/fable-hero-4.png" alt="" class="col-span-full" />
+  <p
+    class="col-start-2 text-sm mt-3 mb-8 tracking-wide font-semibold text-slate-500"
+  >
+    Mock-up of the final toy design. Fable consists of a set of square, magnetic
+    tiles, and a companion app which connects to the tiles over bluetooth.
+  </p>
   <article
     class="col-start-2 col-span-1 prose prose-h1:text-4xl prose-h1:font-bold prose-h1:mb-2 prose-p:prose-md prose-h3:font-normal"
   >
@@ -21,23 +28,18 @@
       can improve second language proficiency is outlined in a report that can
       be read here.
     </h3>
-    <p class="mb-8">
-      <span class="font-bold">Time Frame: </span>5 weeks<br />
-      <span class="font-bold"> Team: </span>
-      Paula Castillero, Kyle Li, Irina Barzykina, Anna Soligo and myself.<br />
-      <span class="font-bold"> Role: </span>I was responsible for the initial
+    <div class="flex flex-col space-y-2">
+      <div><span class="font-bold">Time Frame: </span>5 weeks</div>
+      <div><span class="font-bold"> Team: </span>
+      Paula Castillero, Kyle Li, Irina Barzykina, Anna Soligo and myself.</div>
+      
+      <div><span class="font-bold"> Role: </span>I was responsible for the initial
       concept development, user research, user testing, user interface design,
       and design psychology research for development. I am the sole author of
-      the proposed research study to test the toy's efficacy empirically.
-    </p>
+      the proposed research study to test the toy's efficacy empirically.</div>
+    </div>
   </article>
-  <img src="../../assets/fable-hero-4.png" alt="" class="col-span-full" />
-  <p
-    class="col-start-2 text-sm mt-3 tracking-wide font-semibold text-slate-500"
-  >
-    Mock-up of the final toy design. Fable consists of a set of square, magnetic
-    tiles, and a companion app which connects to the tiles over bluetooth.
-  </p>
+
   <article
     class="mb-2 col-start-2 col-span-1 prose prose-h1:text-4xl prose-h1:font-bold prose-h1:mb-2 prose-h1:mt-10 prose-p:prose-md prose-h3:font-normal prose-h4:mb-3"
   >
@@ -95,16 +97,23 @@
         persistent, especially in challenging tasks like language learning.
       </li>
       <li>
-        <span class="font-bold">Parental Value:</span> If parents don’t see the value in learning a new
-        language, a child is less likely to put in the effort to learn it,
-        regardless of their self-efficacy.
+        <span class="font-bold">Parental Value:</span> If parents don’t see the
+        value in learning a new language, a child is less likely to put in the
+        effort to learn it, regardless of their self-efficacy.
       </li>
       <li>
-        <span class="font-bold">Parental Input:</span> The quality and diversity of language that parents use
-        with their children can impact the child’s language development.
-        Activities like reading together can especially help in improving
-        language skills in children.
+        <span class="font-bold">Parental Input:</span> The quality and diversity
+        of language that parents use with their children can impact the child’s
+        language development. Activities like reading together can especially
+        help in improving language skills in children.
       </li>
     </ul>
   </article>
+  <article
+  class="mb-2 col-start-2 col-span-1 prose prose-h1:text-4xl prose-h1:font-bold prose-h1:mb-2 prose-h1:mt-10 prose-p:prose-md prose-h3:font-normal prose-h4:mb-3"
+>
+  <h1>Research and Analysis</h1>
+  <h2>User Research</h2>
+  
+</article>
 </template>
