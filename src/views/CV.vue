c400cb80428df7435554aefb4abd93bfe4ec03a4
Samantha Foong <shenli574@gmail.com>
2024-03-05 13:56:22 +0000

    new CV

--- src/views/CV.vue
+++ src/views/CV.vue
@@ -4,14 +4,14 @@
       class="text-indigo-700 font-medium tracking-widest text-sm hover:underline hover:underline-offset-4"
     >
       <a
-        href="https://drive.google.com/u/0/uc?id=1ICIu-N3n5t_7eTTEURlVi0zjQEPMYhle&export=download"
+        href="https://drive.google.com/uc?id=1QVumHiMiempEYPjQkQbpxRLjX_b6_f7-&export=download"
         download
         >DOWNLOAD MY CV</a
       >
     </div>
 
     <iframe
-      src="https://drive.google.com/file/d/1ICIu-N3n5t_7eTTEURlVi0zjQEPMYhle/preview"
+      src="https://drive.google.com/file/d/1QVumHiMiempEYPjQkQbpxRLjX_b6_f7-/preview"
       height="680"
       allow="autoplay"
       class="mt-4 w-full"
