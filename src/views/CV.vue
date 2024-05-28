fb3c5cb562756af47d990acc0867e07df5ccff6a
SamSSLF <56586268+SamSSLF@users.noreply.github.com>
2024-05-28 17:26:56 +0100

    Updated CV 280524

--- src/views/CV.vue
+++ src/views/CV.vue
@@ -4,14 +4,14 @@
       class="text-indigo-700 font-medium tracking-widest text-sm hover:underline hover:underline-offset-4"
     >
       <a
-        href="https://drive.google.com/uc?id=1QVumHiMiempEYPjQkQbpxRLjX_b6_f7-&export=download"
+        href="https://drive.google.com/uc?id=1DozBp_k4SRb80AvvQsCWigGz814p4PfH&export=download"
         download
         >DOWNLOAD MY CV</a
       >
     </div>
 
     <iframe
-      src="https://drive.google.com/file/d/1QVumHiMiempEYPjQkQbpxRLjX_b6_f7-/preview"
+      src="https://drive.google.com/file/d/1DozBp_k4SRb80AvvQsCWigGz814p4PfH/preview"
       height="680"
       allow="autoplay"
       class="mt-4 w-full"
